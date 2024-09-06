d66339f6496a204a6dbb3d7799873ba8bd49dc90
Okta Pratama <oktaiwp849@gmail.com>
2024-09-06 11:14:08 +0700

    Adding Okta

---
+++ PerkenalanOkta.java
@@ -0,0 +1,16 @@
+public class PerkenalanOkta {
+
+    public static void main(String[] args) {
+        System.out.println("------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi --");
+        System.out.println("------------------------------------------------");
+        System.out.println("Nama    : Okta Indra Wahyu Pratama");
+        System.out.println("NIM     : ");
+        System.out.println("Kelas   : TI 1E");
+        System.out.println("No Absen: 21");
+        System.out.println("Prodi   : D-IV Teknik Informatika");
+        System.out.println("Alamat  : ");
+        System.out.println("No HP   : ");
+        System.out.println("Hobi    : ");
+    }
+}
